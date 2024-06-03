2ebcfd31b9caccb64d01d44380d26501ac3f8a23
Sabashahid689 <163736077+Sabashahid689@users.noreply.github.com>
2024-06-03 23:12:56 +0500

    Add files via upload

---
+++ many.ts
@@ -0,0 +1,24 @@
+         // HomeWork Class
+// Simple Function:-
+//function halffry(salt:number,butter:number){
+    //console.log(1+butter+salt);
+//}
+//halffry(1.5,2) //manaheet
+//halffry(2,3)   //zaigham
+// Arrow Function:-
+//let tea = () => 1+3+5;
+//let response: number = tea ()
+//console.log(response);
+// Normal function:-
+//function greet (name: string,age?:number,classes?:string){
+    //console.log("hello"+ name)
+//}
+//greet("saba");
+// Rest Function:-
+//function greet (name:string,age:number){
+    //console.log("Hello",name,age)
+//}
+//greet("saba",18,)
+
+
+
